7025cc6ab45f59631347d260715c50cb748d0a6d
Galen Curwen-McAdams <galencm@fastmail.com>
2018-09-13 22:58:45 +0000

    Problem: CONTRIBUTING not included in package
    Solution: use a manifest file and include all
    markdown documentation

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
     platforms=["any"],
     license="Mozilla Public License 2.0 (MPL 2.0)",
     include_package_data=True,
-    data_files=[("", ["LICENSE.txt"])],
     url="",
     packages=find_packages(),
     install_requires=[
