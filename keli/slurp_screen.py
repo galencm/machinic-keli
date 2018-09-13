da71e76021f42f935e98f4fa6bc21e82606a9024
Galen Curwen-McAdams <galencm@fastmail.com>
2018-09-13 22:44:52 +0000

    Problem: linting/formatting are a single commit
    Solution: revert and use two commits for linting/formatting
    
    Revert "Problem: code has not been formatted or linted"
    
    This reverts commit 9b56a373ec33ba8763119263844d7789ca926cf1.

--- keli/slurp_screen.py
+++ keli/slurp_screen.py
@@ -7,7 +7,6 @@ import subprocess
 # however, a more cross-window manager (and X/wayland) tool
 # would be better
 
-
 class SlurpScreenshot(SlurpThing):
     def __init__(self, **kwargs):
         super(SlurpScreenshot, self).__init__(**kwargs)
@@ -18,7 +17,6 @@ class SlurpScreenshot(SlurpThing):
         screenshot_file = "/tmp/{}".format(str(uuid.uuid4()))
         subprocess.call(["gnome-screenshot", "-f", screenshot_file])
         return self.file_bytes(screenshot_file)
-
 
 class SlurpScreenshotRegion(SlurpThing):
     def __init__(self, **kwargs):
@@ -31,7 +29,6 @@ class SlurpScreenshotRegion(SlurpThing):
         subprocess.call(["gnome-screenshot", "-a", "-f", screenshot_file])
         return self.file_bytes(screenshot_file)
 
-
 class SlurpScreenshotWindow(SlurpThing):
     def __init__(self, **kwargs):
         super(SlurpScreenshotWindow, self).__init__(**kwargs)
@@ -43,7 +40,6 @@ class SlurpScreenshotWindow(SlurpThing):
         # delay for 5 seconds to allow window to be selected
         subprocess.call(["gnome-screenshot", "-w", "-d", "5", "-f", screenshot_file])
         return self.file_bytes(screenshot_file)
-
 
 class SlurpScreenshotAnimated(SlurpThing):
     def __init__(self, **kwargs):
--- keli/slurp_webcam.py
+++ keli/slurp_webcam.py
@@ -12,35 +12,33 @@ import sys
 import time
 import subprocess
 import fnmatch
+import datetime
 import glob
 from ma_cli import data_models
-
 
 class SlurpWebCam(object):
     def __init__(self, db_host=None, db_port=None, binary_r=None, redis_conn=None):
         if binary_r and redis_conn:
-            self.binary_r = binary_r
-            self.redis_conn = redis_conn
+             self.binary_r = binary_r
+             self.redis_conn = redis_conn
         else:
             if not db_host and not db_port:
-                db_host, db_port = data_models.service_connection()
+                db_host, db_port =  data_models.service_connection()
 
-            self.redis_conn = redis.StrictRedis(
-                host=db_host, port=str(db_port), decode_responses=True
-            )
+            self.redis_conn = redis.StrictRedis(host=db_host, port=str(db_port),decode_responses=True)
             self.binary_r = redis.StrictRedis(host=db_host, port=str(db_port))
 
     def set_setting(self, device, attribute, value, value_flag=None):
 
         set_values = []
 
-        if device == "_":
+        if device == '_':
             device = None
 
         if device is None:
             devices = self.discover()
         else:
-            devices = [{"uid": device}]
+            devices = [{"uid" : device}]
 
         for dev in devices:
             # allow wildcards in attribute
@@ -48,19 +46,19 @@ class SlurpWebCam(object):
             # run set on any that match
             # fnmatch.fnmatch
 
-            attributes = self.get_setting(dev["uid"])[0]
-            for k, v in attributes.items():
-                if fnmatch.fnmatch(k, attribute):
+            attributes = self.get_setting(dev['uid'])[0]
+            for k,v in attributes.items():
+                if fnmatch.fnmatch(k,attribute):
                     # simple inc / dec, assumes int
-                    if value_flag == "+" or value_flag == "add":
-                        current = self.get_setting(dev["uid"], k)[0]
-                        print(current, value)
+                    if value_flag == '+' or value_flag == 'add':
+                        current = self.get_setting(dev['uid'], k)[0]
+                        print(current,value)
                         if current:
                             value = int(value)
                             current = int(current)
                             current += value
-                    elif value_flag == "-" or value_flag == "sub":
-                        current = self.get_setting(dev["uid"], k)[0]
+                    elif value_flag == '-' or value_flag == 'sub':
+                        current = self.get_setting(dev['uid'], k)[0]
                         if current:
                             value = int(value)
                             current = int(current)
@@ -68,7 +66,7 @@ class SlurpWebCam(object):
                     else:
                         current = value
 
-                    self.redis_conn.hmset("state:{}".format(dev["uid"]), {k: current})
+                    self.redis_conn.hmset("state:{}".format(dev['uid']), {k : current})
                     set_values.append(self.get(device, k))
 
         return set_values
@@ -77,23 +75,19 @@ class SlurpWebCam(object):
 
         get_values = []
 
-        if device == "_":
+        if device == '_':
             device = None
 
         if device is None:
             devices = self.discover()
         else:
-            devices = [{"uid": device}]
+            devices = [{"uid" : device}]
 
         for dev in devices:
             if attribute:
-                get_values.append(
-                    self.redis_conn.hget("state:{}".format(dev["uid"]), attribute)
-                )
+                get_values.append(self.redis_conn.hget("state:{}".format(dev['uid']), attribute))
             else:
-                get_values.append(
-                    self.redis_conn.hgetall("state:{}".format(dev["uid"]))
-                )
+                get_values.append(self.redis_conn.hgetall("state:{}".format(dev['uid'])))
         return get_values
 
     def discover(self):
@@ -103,15 +97,7 @@ class SlurpWebCam(object):
         try:
             now = str(datetime.datetime.now())
             for webcam in glob.glob("/dev/video*"):
-                discoverable.append(
-                    {
-                        "name": webcam,
-                        "address": webcam,
-                        "uid": webcam,
-                        "discovery": method,
-                        "lastseen": now,
-                    }
-                )
+                discoverable.append({'name':webcam,'address':webcam,'uid':webcam, 'discovery': method,'lastseen':now})
         except Exception as ex:
             print(ex)
 
@@ -119,7 +105,7 @@ class SlurpWebCam(object):
 
     def slurp(self, device=None, container="glworb"):
 
-        if device == "_":
+        if device == '_':
             device = None
 
         if device is None:
@@ -134,29 +120,29 @@ class SlurpWebCam(object):
 
             # check using 'in' to allow combinations
             # for example: file+glworb
-            if "file" in container:
+            if 'file' in container:
                 fname = "/tmp/{}.slurp".format(time.time())
-                with open(fname, "wb+") as f:
+                with open(fname,'wb+') as f:
                     f.write(slurped_bytes)
                 slurped.append(fname)
 
-            if "blob" in container:
+            if 'blob' in container:
                 slurped.append(slurped_bytes)
 
-            if "glworb" in container:
+            if 'glworb' in container:
                 blob_uuid = str(uuid.uuid4())
-                blob_uuid = "binary:" + blob_uuid
+                blob_uuid = "binary:"+blob_uuid
                 self.binary_r.set(blob_uuid, slurped_bytes)
 
                 glworb = {}
-                glworb["uuid"] = str(uuid.uuid4())
-                glworb["slurp_source_uid"] = device["uid"]
-                glworb["slurp_method"] = "slurp_webcam"
-                glworb["slurp_source_name"] = device["name"]
-                glworb["binary_key"] = blob_uuid
-                glworb["created"] = str(datetime.datetime.now())
+                glworb['uuid'] = str(uuid.uuid4())
+                glworb['slurp_source_uid'] = device['uid']
+                glworb['slurp_method'] = "slurp_webcam"
+                glworb['slurp_source_name'] =  device['name']
+                glworb['binary_key'] = blob_uuid
+                glworb['created'] = str(datetime.datetime.now())
 
-                glworb_uuid = "glworb:{}".format(glworb["uuid"])
+                glworb_uuid = "glworb:{}".format(glworb['uuid'])
                 self.redis_conn.hmset(glworb_uuid, glworb)
 
                 slurped.append(glworb_uuid)
@@ -168,20 +154,9 @@ class SlurpWebCam(object):
         try:
             tmp_output_filename = "/tmp/slurp_webcam.jpg"
             print(device)
-            # name = device["name"]
-            addr = device["address"]
-            subprocess.call(
-                [
-                    "fswebcam",
-                    "--no-banner",
-                    "--save",
-                    tmp_output_filename,
-                    "-d",
-                    addr,
-                    "-r",
-                    "1280x960",
-                ]
-            )
+            name = device['name']
+            addr = device['address']
+            subprocess.call(["fswebcam", "--no-banner","--save", tmp_output_filename, "-d", addr,"-r", "1280x960"])
             contents = io.BytesIO()
 
             with open(tmp_output_filename, "rb") as f:
